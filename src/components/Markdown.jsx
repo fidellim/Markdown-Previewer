7d06df902465f5ba20f13707c883c121183a299d
fidellim <lim.fidel@gmail.com>
2022-10-05 00:34:48 +0400

    make it responsive

--- src/components/Markdown.jsx
+++ src/components/Markdown.jsx
@@ -1,18 +1,20 @@
 import { useEffect, useState, useRef } from 'react'
 import sanitizeHtml from 'sanitize-html'
 import { marked } from 'marked'
+import { AiOutlineEyeInvisible, AiOutlineEye } from 'react-icons/ai'
 
 const Markdown = () => {
     const [text, setText] = useState('')
+    const [isPreviewerOpen, setIsPreviewerOpen] = useState(false)
     const markedTexts = useRef()
-
-    // img by default is not added in sanitizeHtml
-    const allowedTags = {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
-    }
 
     useEffect(() => {
         // console.log(sanitizeHtml(marked.parse(text)))
+        // img by default is not added in sanitizeHtml
+        const allowedTags = {
+            allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+        }
+
         markedTexts.current.innerHTML = sanitizeHtml(
             marked.parse(text),
             allowedTags
@@ -23,12 +25,28 @@ const Markdown = () => {
         setText(event.target.value)
     }
 
+    const openPreviewer = () => {
+        setIsPreviewerOpen(true)
+    }
+
+    const closePreviewer = () => {
+        setIsPreviewerOpen(false)
+    }
+
     return (
-        <main className="flex gap-1 w-screen h-screen">
-            <div className="flex-1 bg-black">
-                <h1 className="bg-grayishBlack text-white px-3 py-2">
-                    Markdown
-                </h1>
+        <main className="flex flex-col md:flex-row md:gap-1 w-screen h-screen">
+            <div
+                className={`flex-1 bg-black min-h-full md:block ${
+                    isPreviewerOpen && 'hidden'
+                }`}
+            >
+                <div className="flex items-center justify-between bg-grayishBlack text-white px-3 py-2">
+                    <h1>Markdown</h1>
+                    <AiOutlineEye
+                        onClick={openPreviewer}
+                        className="md:hidden"
+                    />
+                </div>
                 <textarea
                     id="style-1"
                     className=" block w-full h-[calc(100%_-_40px)] px-3 py-2 bg-black text-white"
@@ -37,10 +55,18 @@ const Markdown = () => {
                     placeholder="Write your markdown here."
                 />
             </div>
-            <div className="flex-1 bg-black">
-                <h1 className="bg-grayishBlack text-white px-3 py-2">
-                    Preview
-                </h1>
+            <div
+                className={`flex-1 bg-black min-h-full ${
+                    isPreviewerOpen ? 'block' : 'hidden'
+                } md:block`}
+            >
+                <div className="flex items-center justify-between bg-grayishBlack text-white px-3 py-2">
+                    <h1>Preview</h1>
+                    <AiOutlineEyeInvisible
+                        onClick={closePreviewer}
+                        className="md:hidden"
+                    />
+                </div>
                 <div
                     id="style-1"
                     className="px-3 py-2 w-full h-[calc(100%_-_40px)] overflow-auto text-white markdown-body"
